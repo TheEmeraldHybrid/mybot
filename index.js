2b64278113b2e6ed383dd6e249a7b5a4b6e99e0f
TheEmeraldHybrid <37817754+TheEmeraldHybrid@users.noreply.github.com>
2018-03-30 07:48:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,22 +14,6 @@ bot.on("ready", function(){
     console.log("Ready");
 });
 
-//ffmpeg
-var ffmpeg = require('ffmpeg');
-
-try {
-	new ffmpeg('/path/to/your_movie.avi', function (err, video) {
-		if (!err) {
-			console.log('The video is ready to be processed');
-		} else {
-			console.log('Error: ' + err);
-		}
-	});
-} catch (e) {
-	console.log(e.code);
-	console.log(e.msg);
-}
-
 //hey command
 
 bot.on("message", function(message){
